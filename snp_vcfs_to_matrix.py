547270f8863cc8f03cba6bb33bb15757e9786bb1
oscarlr <olrodriguez01@gmail.com>
2021-01-26 10:12:51 -0500

    add

--- snp_vcfs_to_matrix.py
+++ snp_vcfs_to_matrix.py
@@ -11,6 +11,7 @@ def transform_genotype(genotype):
     function = {
         "0/0": "0",
         "0/1": "1",
+        "1/0": "1",
         "1/1": "2"
     }
     if genotype in function:
@@ -18,24 +19,30 @@ def transform_genotype(genotype):
     return t
     
 def vcf_genotypes(fn,chrom="igh"):
-    vcf_reader = vcf.Reader(open(fn, 'r'))
-    sample = vcf_reader.samples[0]
-    genotypes = {"pos": [],sample: []}
-    for record in vcf_reader:
-        if record.CHROM != chrom:
-            continue
-        genotype = record.genotype(sample)['GT']
-        transformed_gt = transform_genotype(genotype)
-        if transformed_gt == None:
-            continue
-        genotypes["pos"].append(record.POS)
-        genotypes[sample].append(transformed_gt)
-    return pd.DataFrame(genotypes)
+    vcf_reader = vcf.Reader(open(fn, 'r'))    
+    sample_genotypes = None
+    for i,sample in enumerate(vcf_reader.samples):
+        vcf_reader2 = vcf.Reader(open(fn, 'r'))
+        genotypes = {"pos": [],sample: []}
+        for record in vcf_reader2:
+            if record.CHROM != chrom:
+                continue
+            genotype = record.genotype(sample)['GT']
+            transformed_gt = transform_genotype(genotype)
+            if transformed_gt == None:
+                continue
+            genotypes["pos"].append(record.POS)
+            genotypes[sample].append(transformed_gt)
+        if i == 0:
+            sample_genotypes = pd.DataFrame(genotypes)
+        else:
+            genotypes = pd.DataFrame(genotypes)
+            sample_genotypes = sample_genotypes.merge(genotypes,how="outer",on="pos")
+    return sample_genotypes
 
 vcf_fofh = open(vcf_fofn,'r')
 for i,fn in enumerate(vcf_fofh):
     fn = fn.rstrip()
-    print fn
     fn_genotypes = vcf_genotypes(fn)
     if i == 0:
         genotypes = fn_genotypes
